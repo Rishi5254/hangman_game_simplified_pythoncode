5834d80a0236ba3cef6eaf1675f4db552c228b6d
Rishi5254 <94619066+Rishi5254@users.noreply.github.com>
2021-11-24 18:41:17 +0530

    Update data.py

--- data.py
+++ data.py
@@ -2,6 +2,17 @@
 word_list = ['apple', 'banana', 'mango', 'strawberry', 'orange', 'grape', 'pineapple', 'apricot',
              'lemon', 'coconut', 'watermelon', 'cherry', 'papaya', 'berry', 'peach', 'lychee', 'muskmelon']
 
+logo = ''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
+
+
 stages = ['''
   +---+
   |   |
